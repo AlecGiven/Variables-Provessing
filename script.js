57cba9caf04e4cbfe8da8cca0932cba7c5df784f
AlecGiven <78773178+AlecGiven@users.noreply.github.com>
2021-02-22 15:52:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var containerEle = document.body.querySelector(".container");
 
 var num1 = Number(prompt("What is your total bill"));
-var num2 = Number(prompt("What is the tax percentage"));
-var num3 = Number(prompt("What is the tip percentage"));
+var num2 = 0.07
+var num3 = 0.05
 
 var sum = (num1 * num2) + num1 * num3 + num1;
 
